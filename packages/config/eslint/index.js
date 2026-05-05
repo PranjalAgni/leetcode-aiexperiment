ba3b22c122cab69f8016387671b88f4a2478925b
PranjalAgni <pranjal.agnihotri6@gmail.com>
2026-05-05 15:45:57 +0530

    fix: add ESLint 9 flat config to all Node services and wire up typescript-eslint plugin

--- packages/config/eslint/index.js
+++ packages/config/eslint/index.js
@@ -1,11 +1,24 @@
+const tsPlugin = require('@typescript-eslint/eslint-plugin')
+const tsParser = require('@typescript-eslint/parser')
+
 /** @type {import("eslint").Linter.Config[]} */
 module.exports = [
   {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    languageOptions: {
+      parser: tsParser,
+    },
     rules: {
       'no-console': ['warn', { allow: ['warn', 'error'] }],
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/consistent-type-imports': 'error',
     },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**'],
   },
 ]
